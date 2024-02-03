673cd55b73b0adae2b0fa41837434dddc8f0f371
nashs789 <nashs789@naver.com>
2024-02-03 09:45:28 +0900

    feat: another solution

--- LIB/2024.02.03/Q9251_LCS/Q9251.py
+++ LIB/2024.02.03/Q9251_LCS/Q9251.py
@@ -10,3 +10,19 @@ for i in range(1, len1):
       LCS[i][j] = max(LCS[i - 1][j], LCS[i][j - 1])
 
 print(LCS[len1 - 1][len2 - 1])
+
+# import sys
+# read = sys.stdin.readline
+
+# word1, word2 = read().strip(), read().strip()
+# l1, l2 = len(word1), len(word2)
+# cache = [0] * l2
+
+# for i in range(l1):
+#     cnt = 0
+#     for j in range(l2):
+#         if cnt < cache[j]:
+#             cnt = cache[j]
+#         elif word1[i] == word2[j]:
+#             cache[j] = cnt + 1
+# print(max(cache))
